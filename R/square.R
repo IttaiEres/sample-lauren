93bf9a810df65edabff440ceaf6e0bfd0ce18a90
Ittai Eres <ittai.eres@gmail.com>
2015-07-23 13:51:18 -0500

    Destroy everything.

--- R/square.R
+++
@@ -1,7 +0,0 @@
-args <- strtoi(commandArgs(TRUE))
-
-square <- function(x) {
-  return(x * (x))
-}
-
-print(paste0("Your answer is: ", toString(square(args[[1]]))))
--- README.md
+++
@@ -1,5 +0,0 @@
-# Smart Squaring
-
-Square numbers intelligently. Basically, $ x^2 = x * x $.
-
-(c) 2015, Raman A. Shah.
